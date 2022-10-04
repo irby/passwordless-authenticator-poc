c5c8fb7d099d380a7f64b481de047b543709d1d3
Matthew H. Irby <matt.irby@outlook.com>
2022-10-03 20:58:47 -0700

    Setup test for handler

--- authenticator-backend/handler/account_sharing_test.go
+++ authenticator-backend/handler/account_sharing_test.go
@@ -2,4 +2,14 @@ package handler
 
 import (
 	_ "github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/teamhanko/hanko/backend/config"
+	"github.com/teamhanko/hanko/backend/test"
+	"testing"
 )
+
+func TestNewAccountSharingHandler(t *testing.T) {
+	handler, err := NewAccountSharingHandler(&config.Config{}, test.NewPersister(nil, nil, nil, nil, nil, nil, nil), sessionManager{}, mailer{})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, handler)
+}
